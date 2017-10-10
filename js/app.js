caeb655e534b1671771ce9a9318a17b966bcf626
Rocciescobar <roccisen.88@gmail.com>
2017-10-10 12:39:38 -0500

    Actualizando index.html y app.js

--- js/app.js
+++ js/app.js
@@ -1,45 +1,71 @@
-//Pidiendo una frase al usuario
-cipher (prompt ("Ingrese una frase a cifrar"))
-decipher (prompt ("Ingrese una frase a descifrar"))
+//1. Cifrando frase
 
-// 1. Cifrando frase
+//Creando función para cifrar
 function cipher (phrase) {
+
+  //Pidiendo una frase al usuario
+  var phrase = document.getElementsByTagName("input")[0].value;
 
   //Obtener el caracter ASCII de cada letra y almacenar en un array
   var inputPhrase = [];
   for (i = 0; i < phrase.length; i++) {
     inputPhrase.push(phrase.charCodeAt(i));
-
-    //Aplicar fórmula de Cifrado César, obtener letra de cada caracter ASCII y almacenar en un array
-    var outputPhrase = [];
-    for (j = 0; j < phrase.length; j++) {
-      outputPhrase.push(String.fromCharCode((inputPhrase[j] - 65 + 33) % 26 + 65));
-
-    }
   }
 
+    //Aplicar fórmula
+    for (j = 0; j < phrase.length; j++) {
+        if (inputPhrase[j]>= 65 && inputPhrase[j]<= 90){ //Para letras MAYUSCULAS
+            inputPhrase[j] = (inputPhrase[j] - 65 + 33)%26 + 65
+        }else if (inputPhrase[j]>= 97 && inputPhrase[j]<= 122){
+            inputPhrase[j] = (inputPhrase[j] - 97 + 33)%26 + 97
+        }else{
+            inputPhrase[j] = inputPhrase [j]
+        }
+    }
+
+    //Transformar número en letra (Caracter ASCII)
+    var outputPhrase = [];
+    for (j = 0; j < phrase.length; j++) {
+      outputPhrase.push(String.fromCharCode(inputPhrase[j]))
+    }
+
   //Convertir el array en string y mostrar frase cifrada al usuario
-  document.write("La frase cifrada es " + outputPhrase.join(''))
+  document.getElementById('resultado').innerHTML = outputPhrase.join('')
 
 }
 
 //2. Descifrando frase
-function decipher (phrase1) {
+
+//Creando función para descifrar
+function decipher (phrase) {
+
+//Pidiendo una frase al usuario
+var phrase = document.getElementsByTagName("input")[0].value;
 
   //Obtener el caracter ASCII de cada letra y almacenar en un array
-  var inputPhrase1 = [];
-  for (i = 0; i < phrase1.length; i++) {
-    inputPhrase1.push(phrase1.charCodeAt(i));
-
-    //Aplicar fórmula de Cifrado César, obtener letra de cada caracter ASCII y almacenar en un array
-    var outputPhrase1 = [];
-    for (j = 0; j < phrase1.length; j++) {
-      outputPhrase1.push(String.fromCharCode((inputPhrase1[j] - 65 - 33) % 26 + 65));
-
-    }
+  var inputPhrase = [];
+  for (i = 0; i < phrase.length; i++) {
+    inputPhrase.push(phrase.charCodeAt(i));
   }
 
+    //Aplicar fórmula
+    for (j = 0; j < phrase.length; j++) {
+        if (inputPhrase[j]>= 65 && inputPhrase[j]<= 90){ //Para letras MAYUSCULAS
+            inputPhrase[j] = (- 33)%26 + inputPhrase[j]
+        }else if (inputPhrase[j]>= 97 && inputPhrase[j]<= 122){
+            inputPhrase[j] = (- 33)%26 + inputPhrase[j]
+        }else{
+            inputPhrase[j] = inputPhrase [j]
+        }
+    }
+
+    //Transformar número en letra (Caracter ASCII)
+    var outputPhrase = [];
+    for (j = 0; j < phrase.length; j++) {
+      outputPhrase.push(String.fromCharCode(inputPhrase[j]))
+    }
+
   //Convertir el array en string y mostrar frase cifrada al usuario
-  document.write("La frase descifrada es " + outputPhrase1.join(''))
+  document.getElementById('resultado').innerHTML = outputPhrase.join('')
 
 }
